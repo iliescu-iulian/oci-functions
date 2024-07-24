b46f88686f3d057e87ca7b4858098f9dc7cd0a36
Iliescu,Iulian <iliescu_iulian@yahoo.com>
2024-07-24 12:05:47 +0300

    * move secrets in main

--- vault-poc/src/Function/Program.cs
+++ vault-poc/src/Function/Program.cs
@@ -1,6 +1,10 @@
 using Fnproject.Fn.Fdk;
 using NLog;
+using Oci.Common;
 using Oci.Common.Auth;
+using Oci.SecretsService;
+using Oci.SecretsService.Models;
+using Oci.SecretsService.Requests;
 using System;
 using System.Runtime.CompilerServices;
 
@@ -12,10 +16,8 @@ namespace Function
     {
 
         private ResourcePrincipalAuthenticationDetailsProvider _provider;
-        private string _namespace;
-        private string _compartmentId;
+        private SecretsClient _secretsClient;
         private NLog.Logger _logger;
-        private Secrets _secrets;
 
         public Vault()
         {
@@ -25,33 +27,44 @@ namespace Function
             NLog.LogManager.Configuration = config;
             _logger = NLog.LogManager.GetCurrentClassLogger();
 			_provider= ResourcePrincipalAuthenticationDetailsProvider.GetProvider();
-            _namespace = Environment.GetEnvironmentVariable("NAMESPACE");
-            _compartmentId= Environment.GetEnvironmentVariable("COMPARTMENT");
+            _secretsClient = new SecretsClient(_provider);
+            
+        }
+
+        public string handleRequest(string id)
+        {
             try
             {
-                _secrets = new Secrets(_provider);
-                Console.WriteLine("Vault.Constructor: Key='{0}', Secrets='{1}'",
-                    _secrets.ConsumerKey, _secrets.ConsumerSecret);
+                if (string.IsNullOrEmpty(id))
+                {
+                    var secret = GetSecret(Environment.GetEnvironmentVariable("CDI_CONSUMERSECRET_OCID"));
+                    var key = GetSecret(Environment.GetEnvironmentVariable("CDI_CONSUMERKEY_OCID"));
+                    _logger.Info($"Secrets Key={key} Secret={secret}");
+                    return key;
+                }
+
+                var value = GetSecret(id);
+                _logger.Info($"Secret ID={id} value={value}");
+                return value;
             }
             catch (Exception e)
             {
                 _logger.Error(e);
-                Console.WriteLine("Secrets failure: {0} / {1}", e.Message, e.StackTrace);
+                return e.Message;
             }
         }
 
-        public string handleRequest(string eventData)
+        private string GetSecret(string ocid)
         {
-            if(_secrets != null)
+            var req = new GetSecretBundleRequest
             {
-                _logger.Info($"Secrets Key={_secrets.ConsumerKey} Secret={_secrets.ConsumerSecret}");
-                return "Secrets Key={_secrets.ConsumerKey} Secret={_secrets.ConsumerSecret}";
-            }
-            _logger.Warn("Secret instance not available");
-            return "Secret instance not available";
+                SecretId = ocid
+            };
+            var resp = _secretsClient.GetSecretBundle(req).Result;
+            Base64SecretBundleContentDetails secretValue = resp.SecretBundle.SecretBundleContent as Base64SecretBundleContentDetails;
+            return secretValue?.Content;
         }
 
-        
 
         static void Main(string[] args) { Fdk.Handle(args[0]); }
 	}
